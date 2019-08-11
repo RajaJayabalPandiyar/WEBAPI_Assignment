18dd9e1c2ce8f2872405801ff68ed1d64968353b
RajaJayabalPandiyar <53605690+RajaJayabalPandiyar@users.noreply.github.com>
2019-08-11 18:57:58 +0800

    WEB API - Assignment upload
    WEB API - Assignment upload

---
+++ Controllers/PurchaseOrderController.cs
@@ -0,0 +1,69 @@
+﻿using PurchaseOrderService.BusinessLayer;
+using PurchaseOrderService.DataContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIAssignment.Controllers
+{
+    public class PurchaseOrderController : ApiController
+    {
+        PurchaseOrderBusinessLayer poBusinesssLayer = new PurchaseOrderBusinessLayer();
+
+        [Route("searchPurchaseOrder")]
+        [HttpGet]
+        public IHttpActionResult Search(string purchaseOrderNo)
+        {
+            try
+            {
+                var suppliers = poBusinesssLayer.GetPurchaseOrder(purchaseOrderNo);
+
+                return Json(suppliers);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [Route("addPurchaseOrder")]
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]PurchaseOrder purchaseOrder)
+        {
+            return Json(new
+            {
+                returnString = poBusinesssLayer.AddPurchaseOrder(purchaseOrder)
+            });
+
+        }
+
+        [Route("updatePurchaseOrder")]
+        [HttpPost]
+        public IHttpActionResult Put([FromBody]PurchaseOrder purchaseOrder)
+        {
+            var returnString = poBusinesssLayer.UpdatePurchaseOrder(purchaseOrder);
+
+            return Json(new
+            {
+                returnString = returnString
+            });
+
+        }
+
+        [Route("deletePurchaseOrder")]
+        [HttpPost]
+        public IHttpActionResult Delete([FromBody]PurchaseOrder purchaseOrder)
+        {
+            var returnString = poBusinesssLayer.DeletePurchaseOrder(purchaseOrder);
+
+            return Json(new
+            {
+                returnString = returnString
+            });
+        }
+    }
+}
